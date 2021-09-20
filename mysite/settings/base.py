cb847eac24f29df8ed11e06edf04dca1f873cf3e
wayfrr <wayfrr@gmail.com>
2021-09-20 08:53:52 +0530

    Upto and including Blog Detail Pages

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -28,9 +28,14 @@ INSTALLED_APPS = [
     'search',
     'flex',
     'streams',
+    'sitesettings',
+    'subscribers',
+    'blog',
 
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
+    'wagtail.contrib.settings',
+    'wagtail.contrib.modeladmin',
     'wagtail.embeds',
     'wagtail.sites',
     'wagtail.users',
@@ -79,6 +84,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'wagtail.contrib.settings.context_processors.settings',
             ],
         },
     },
@@ -162,3 +168,5 @@ WAGTAIL_SITE_NAME = "mysite"
 # Base URL to use when referring to full URLs within the Wagtail admin backend -
 # e.g. in notification emails. Don't include '/admin' or a trailing slash
 BASE_URL = 'http://example.com'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
