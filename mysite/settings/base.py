97e3052c8f87c747de48d4e0be50603add6a14f3
wayfrr <wayfrr@gmail.com>
2021-09-21 12:34:24 +0530

    including Snippet ChooserPanel to Sel

--- mysite/settings/base.py
+++ mysite/settings/base.py
@@ -35,6 +35,8 @@ INSTALLED_APPS = [
     'wagtail.contrib.forms',
     'wagtail.contrib.redirects',
     'wagtail.contrib.settings',
+    'wagtail.contrib.routable_page',
+    'wagtail.contrib.sitemaps',
     'wagtail.contrib.modeladmin',
     'wagtail.embeds',
     'wagtail.sites',
@@ -55,6 +57,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
